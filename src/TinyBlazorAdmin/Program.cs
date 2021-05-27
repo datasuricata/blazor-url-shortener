7097d9eaa5fb4942cae1e283895355780d16fcd9
lucas.moraes.dev@gmail.com <lucas.moraes.dev@gmail.com>
2021-05-27 16:17:01 -0300

    removido autenticação

--- src/TinyBlazorAdmin/Program.cs
+++ src/TinyBlazorAdmin/Program.cs
@@ -16,25 +16,30 @@ namespace TinyBlazorAdmin
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
-            // set up a delegate to get function token
+            //var config = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+            //var section = config.GetSection(nameof(UrlShortenerSecuredService));
+            //var endpoint = section.GetValue<string>("Endpoint");
+
+
+            //set up a delegate to get function token
             static string functionEndpoint(WebAssemblyHostBuilder builder) =>
                 builder.Configuration
                     .GetSection(nameof(UrlShortenerSecuredService))
-                    .GetValue<string>(nameof(AzFuncAuthorizationMessageHandler.Endpoint));
+                    .GetValue<string>("Endpoint");
 
             // sets up AAD + user_impersonation to access functions.
-            builder.Services.AddMsalAuthentication(options =>
-            {
-                options.ProviderOptions
-                .DefaultAccessTokenScopes.Add("user.read");
-                options.ProviderOptions
-                .AdditionalScopesToConsent.Add($"{functionEndpoint(builder)}user_impersonation");
-                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
-            });
+            //builder.Services.AddMsalAuthentication(options =>
+            //{
+            //    options.ProviderOptions
+            //    .DefaultAccessTokenScopes.Add("user.read");
+            //    options.ProviderOptions
+            //    .AdditionalScopesToConsent.Add($"{functionEndpoint(builder)}user_impersonation");
+            //    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+            //});
 
 
             // set up DI
-            builder.Services.AddTransient<AzFuncAuthorizationMessageHandler>();
+            //builder.Services.AddTransient<AzFuncAuthorizationMessageHandler>();
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             // configure the client to talk to the Azure Functions endpoint.
@@ -42,7 +47,7 @@ namespace TinyBlazorAdmin
                 client =>
                 {
                     client.BaseAddress = new Uri(functionEndpoint(builder));
-                }).AddHttpMessageHandler<AzFuncAuthorizationMessageHandler>();
+                });/*.AddHttpMessageHandler<AzFuncAuthorizationMessageHandler>();*/
 
             builder.Services.AddTransient<UrlShortenerSecuredService>();
             builder.Services.AddTransient<AzFuncClient>();
